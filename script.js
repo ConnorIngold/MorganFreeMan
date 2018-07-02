5a198f89e9fbbb1cc7a2b1cba1215d94d51a9b2c
connor ingold <connoringold@gmail.com>
2018-07-02 22:11:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// const sound = document.getElementById('button');
+// var snd = new Audio('truth.mp3');
+//
+// console.log('hi');
+// sound.addEventListener("click", playSound)
+//
+// function playSound(){
+//   play.snd()
+//   return playSound
+// }
